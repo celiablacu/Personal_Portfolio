e07404e43b075a6c15339bfacd7ffd300dd5ca1f
Celia Blacu <celia.blacu@gmail.com>
2021-10-07 22:31:27 +0300

    Version 1.0

---
+++ portfolio/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here -- classes
+
+class Project(models.Model):
+    #Check Django documenations on Field
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
